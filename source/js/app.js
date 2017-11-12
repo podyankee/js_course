db26413debd18b9f70a6e74d480b0cbbc89a605d
podyankee <podyankee@gmail.com>
2017-11-13 01:57:31 +0300

    lesson 75

--- source/js/app.js
+++ source/js/app.js
@@ -98,6 +98,15 @@ var UIController = (function() {
             // Insert the HTML in to the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+        clearFields: function () {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue)
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+             fieldsArr[0].focus();
+        },
         getDomstrings: function() {
             return DOMstrings;
         }
@@ -133,8 +142,10 @@ var controller = (function (budgetCtrl, UICtrl) {
        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
        // 3. Add the item to the UI
        UICtrl.addListItem(newItem, input.type);
-       // 4. Calculate the budget
-       // 5. Display the budget on the UI
+       // 4. Clear the fields
+       UICtrl.clearFields();
+       // 5. Calculate the budget
+       // 6. Display the budget on the UI
        
    };
 
